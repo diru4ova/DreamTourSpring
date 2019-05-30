86f64520b4bd0b2fabb51ba0a323360340aaf750
Olena <diru4ova@gmail.com>
2019-05-30 13:03:49 +0300

    init project

---
+++ src/main/java/com/softserve/academy/config/DBConfig.java
@@ -0,0 +1,34 @@
+package com.softserve.academy.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+//@ComponentScan({ "com.softserve.academy.entity" })
+public class DBConfig {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        factoryBean.setConfigLocation(context.getResource("classpath:persistance.xml"));
+        factoryBean.setPackagesToScan("com.softserve.academy.entity");
+        //factoryBean.setAnnotatedClasses();
+        return factoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager getTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(getSessionFactory().getObject());
+        return transactionManager;
+    }
+}
