de1fe756d9ca8898a5bd666fefad26cb8310e88e
Rostyk Hlynka <hlynka1608@gmail.com>
2019-06-01 20:29:19 +0300

    remove imports

--- src/main/java/com/softserve/academy/dreamtourspring/dao/implementations/VisaDaoImpl.java
+++ src/main/java/com/softserve/academy/dreamtourspring/dao/implementations/VisaDaoImpl.java
@@ -6,10 +6,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
