b60a0dc32e75c6d505836748052f99fbc5fdbf4b
Rostyk Hlynka <hlynka1608@gmail.com>
2019-06-01 20:27:56 +0300

    @transactional

--- src/main/java/com/softserve/academy/dreamtourspring/service/implementations/VisaServiceImpl.java
+++ src/main/java/com/softserve/academy/dreamtourspring/service/implementations/VisaServiceImpl.java
@@ -5,11 +5,13 @@ import com.softserve.academy.dreamtourspring.model.Visa;
 import com.softserve.academy.dreamtourspring.service.interfaces.IVisaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Service
+@Transactional
 public class VisaServiceImpl implements IVisaService {
 
     @Autowired
