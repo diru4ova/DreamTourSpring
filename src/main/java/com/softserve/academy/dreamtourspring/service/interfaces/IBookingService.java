669cbaf3da230427c99729e87b27cfc177f978da
DaNkOLULzz <danylolototskyi@gmail.com>
2019-06-05 12:03:11 +0300

    add javadoc to hotel, room, booking service

--- src/main/java/com/softserve/academy/dreamtourspring/service/interfaces/IBookingService.java
+++ src/main/java/com/softserve/academy/dreamtourspring/service/interfaces/IBookingService.java
@@ -4,18 +4,54 @@ import com.softserve.academy.dreamtourspring.model.Booking;
 
 import java.util.List;
 
+/**
+ * Booking dao implementation
+ *
+ * @author Danylo Lototskyi
+ */
 public interface IBookingService {
 
+    /**
+     * Find all instances of booking
+     *
+     * @return list of instances
+     */
     List<Booking> getAll();
 
+    /**
+     * Makes given instance persistent.
+     *
+     * @param booking instance to be persisted
+     */
     void add(Booking booking); // C
 
+    /**
+     * Find booking by id
+     *
+     * @param id booking id
+     * @return found booking
+     */
     Booking get(int id); // R
 
+    /**
+     * Update given booking
+     *
+     * @param booking instance to be updated
+     */
     void update(Booking booking); // U
 
+    /**
+     * Delete booking by id
+     *
+     * @param id booking id
+     */
     void delete(int id); // D
 
+    /**
+     * Find all bookings of particular person
+     * @param idPerson person id for finding bookings
+     * @return booking list of person
+     */
     List<Booking> getAllByPerson(int idPerson);
 
 }
