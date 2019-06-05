8cf57941142582ac8bd37d577fe670f75c6b594e
DaNkOLULzz <danylolototskyi@gmail.com>
2019-06-05 11:27:46 +0300

    add javadoc to hotel, booking and room dao

--- src/main/java/com/softserve/academy/dreamtourspring/dao/interfaces/IBookingDao.java
+++ src/main/java/com/softserve/academy/dreamtourspring/dao/interfaces/IBookingDao.java
@@ -4,8 +4,18 @@ import com.softserve.academy.dreamtourspring.model.Booking;
 
 import java.util.List;
 
+/**
+ * Boooking dao
+ *
+ * @author Danylo Lototskyi
+ */
 public interface IBookingDao extends IDao<Booking> {
 
+    /**
+     * Find all bookings of particular person
+     * @param idPerson person id for finding bookings
+     * @return booking list of person
+     */
     List<Booking> getAllByPerson(int idPerson);
 
 }
--- src/main/java/com/softserve/academy/dreamtourspring/dao/interfaces/IRoomDao.java
+++ src/main/java/com/softserve/academy/dreamtourspring/dao/interfaces/IRoomDao.java
@@ -4,8 +4,20 @@ import com.softserve.academy.dreamtourspring.model.Room;
 
 import java.util.List;
 
+/**
+ * Room dao
+ *
+ * @author Danylo Lototskyi
+ */
 public interface IRoomDao extends IDao<Room> {
 
+    /**
+     * Find all available rooms in hotel on chosen period
+     * @param startDate start date of period
+     * @param endDate end date of period
+     * @param idHotel hotel id for finding free rooms
+     * @return list of free rooms it this hotel
+     */
     List<Room> getFreeRoomsInHotel(String startDate, String endDate, int idHotel);
 
 }
